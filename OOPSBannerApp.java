c15e12e91718d2754b0d04944967e890d7292cb3
maver1ckz <as9998@srmist.edu.in>
2026-03-10 08:28:53 +0530

    Refactored UC3 to use String.join for memory efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,24 @@
 /**
- * OOPSBannerApp UC2 Banner Display Application
+ * OOPSBannerApp UC3 OOPS Banner Application (Use Case 3)
  *
- * This class demonstrates the creation of a visual banner displaying the word "OOPS"
- * using ASCII art composed of asterisks (*) and spaces. The banner is printed to the
- * console using individual print statements for each line.
+ * This class extends the functionality of Use Case 2 by improving the way the "OOPS" banner
+ * is constructed and displayed. It focuses on enhancing code readability and efficiency
+ * by utilizing the String.join() method to create each line of the banner. This approach
+ * overcomes the drawbacks of using the + operator for string concatenation, which can lead
+ * to code inefficiency due to the creation of multiple intermediate String objects in memory.
  *
  * @author Developer
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // Line 1
-        System.out.println("  *** " + "   " + "  *** " + "   " + "****** " + "   " + " ***** ");
-        // Line 2
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + "** **" + "   " + "** ");
-        // Line 3
-        System.out.println("** **" + "   " + "** **" + "   " + "** **" + "   " + "** ");
-        // Line 4
-        System.out.println("** **" + "   " + "** **" + "   " + "****** " + "   " + " ***** ");
-        // Line 5
-        System.out.println("** **" + "   " + "** **" + "   " + "** " + "   " + "     **");
-        // Line 6
-        System.out.println(" ** ** " + "   " + " ** ** " + "   " + "** " + "   " + "     **");
-        // Line 7
-        System.out.println("  *** " + "   " + "  *** " + "   " + "** " + "   " + " ***** ");
+        // Construct each line using String.join() method with "" as the delimiter
+        System.out.println(String.join("", "  *** ", "   ", "  *** ", "   ", "****** ", "   ", " ***** "));
+        System.out.println(String.join("", " ** ** ", "   ", " ** ** ", "   ", "** **", "   ", "** "));
+        System.out.println(String.join("", "** **", "   ", "** **", "   ", "** **", "   ", "** "));
+        System.out.println(String.join("", "** **", "   ", "** **", "   ", "****** ", "   ", " ***** "));
+        System.out.println(String.join("", "** **", "   ", "** **", "   ", "** ", "   ", "     **"));
+        System.out.println(String.join("", " ** ** ", "   ", " ** ** ", "   ", "** ", "   ", "     **"));
+        System.out.println(String.join("", "  *** ", "   ", "  *** ", "   ", "** ", "   ", " ***** "));
     }
 }
